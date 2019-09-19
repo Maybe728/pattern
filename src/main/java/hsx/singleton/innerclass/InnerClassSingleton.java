7429a4f96f26d3b599e08f4d8ade18db3cb409e6
Maybe728 <1007402145@qq.com>
2019-09-19 16:05:01 +0800

    1.丰富注释

--- src/main/java/hsx/singleton/innerclass/InnerClassSingleton.java
+++ src/main/java/hsx/singleton/innerclass/InnerClassSingleton.java
@@ -1,6 +1,8 @@
 package java.hsx.singleton.innerclass;
 
 /**内部类   线程安全  懒汉式**/
+
+/**完美的解决了饿汉式的内存浪费和synchronized的性能问题**/
 public class InnerClassSingleton {
     private InnerClassSingleton(){
         //防止被反射破坏
@@ -8,9 +10,13 @@ public class InnerClassSingleton {
             throw new RuntimeException("不允许创建多实例");
         }
     }
+
+    /**static 为了使单例内存共享  final为了让方法不被重写，重载 **/
     public static final InnerClassSingleton getInstance(){
         return InnerClassHolder.innerClassSingleton;
     }
+
+    /**默认不加载，使用的时候才会加载**/
     private static class InnerClassHolder{
         private static final InnerClassSingleton innerClassSingleton = new InnerClassSingleton();
     }
